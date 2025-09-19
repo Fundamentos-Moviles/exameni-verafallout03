2c708c9d22ad640b7f50176ea1cc6718081f4d67
verafallout03 <100484560+verafallout03@users.noreply.github.com>
2025-09-19 10:17:43 -0600

    onTileTap modificado

--- lib/memorama.dart
+++ lib/memorama.dart
@@ -13,6 +13,8 @@ class Memorama extends StatefulWidget {
 
 class _MemoramaState extends State<Memorama> {
   late List<ColorTile> tiles;
+  List<int> selected = [];
+  int paresFound = 0;
 
   @override
   void initState() {
@@ -41,9 +43,33 @@ class _MemoramaState extends State<Memorama> {
   }
 
   void _onTileTap(int index) {
+    if (tiles[index].revealed) return;
+
+    if (selected.length == 2) return;
+
     setState(() {
       tiles[index].revealed = !tiles[index].revealed;
+      selected.add(index);
     });
+
+    if (selected.length == 2) {
+      final indexOne = selected[0];
+      final indexTwo = selected[1];
+
+      if (tiles[indexOne].colorEnd == tiles[indexTwo].colorEnd) {
+        paresFound++;
+        selected.clear();
+
+        if (paresFound == 10) {}
+      } else {
+        Future.delayed(const Duration(seconds: 1), () {
+          setState(() {
+            tiles[indexOne].revealed = tiles[indexTwo].revealed = false;
+            selected.clear();
+          });
+        });
+      }
+    }
   }
 
   @override
