78f61b87197573d3689cde825906a9fa1dadcdae
verafallout03 <100484560+verafallout03@users.noreply.github.com>
2025-09-19 08:21:15 -0600

    InitState en memorama creado

--- lib/memorama.dart
+++ lib/memorama.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:memoramap1/colortile.dart';
+import 'package:memoramap1/random_color.dart';
 
 class Memorama extends StatefulWidget {
   const Memorama({super.key});
@@ -8,8 +12,31 @@ class Memorama extends StatefulWidget {
 }
 
 class _MemoramaState extends State<Memorama> {
+  late List<ColorTile> tiles;
+
   void initState() {
     super.initState();
+
+    final List<Color> baseColors = [];
+    while (baseColors.length < 10) {
+      final nuevoColor = randomColor();
+      if (!baseColors.contains(nuevoColor)) {
+        baseColors.add(nuevoColor);
+      }
+    }
+
+    tiles =
+        List<ColorTile>.from(
+          baseColors.map(
+            (c) => ColorTile(colorStart: Colors.grey, colorEnd: c),
+          ),
+        )..addAll(
+          baseColors.map(
+            (c) => ColorTile(colorStart: Colors.grey, colorEnd: c),
+          ),
+        );
+
+    tiles.shuffle(Random());
   }
 
   @override
